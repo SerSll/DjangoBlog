9a0dcae3547112b535d3898f13ebb7ca539b152c
sersll <dovakinser@gmail.com>
2019-12-03 11:57:09 +0300

    Add firts app

---
+++ blog/apps/articles/models.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+import datetime
+
+
+class Article(models.Model):
+
+    title = models.CharField('Название статьи', max_length=200)    # поле для ввода небольшого текста
+    text = models.TextField('Содержание статьи')  # Поле для ввода большого текста
+    date_publ = models.DateTimeField('Дата публикации')   # Дата публикации
+    author = models.CharField('Автор', max_length=50)  # Автор статьи
+
+    def __str__(self):
+        return self.title
+
+    def last_7_days(self):
+        return self.date_publ >= (timezone.now() - datetime.timedelta(days = 7))
+
+    class Meta():
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'   # Руссифицируем админку
+
+
+class Comment(models.Model):
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)  # Привязка к статье
+    name_publ = models.CharField('Имя автора комм.', max_length=60)
+    text = models.CharField('Содержание комментария', max_length=200)
+
+    def __str__(self):
+        return self.name_publ
+
+    class Meta():
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'  # Руссифицируем админку
---
+++ blog/apps/articles/views.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from .models import Article
+from django.urls import reverse
+
+def index(request):
+    latest_articles_list = Article.objects.order_by('-date_publ')[:3]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+    latest_comm = a.comment_set.order_by('-id')[:10]
+    return render(request, 'articles/detail.html', {'article':a,'latest_comm':latest_comm})
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+    a.comment_set.create(name_publ = request.POST['name'], text = request.POST['text'])
+    
+    return HttpResponseRedirect( reverse('articles:detail', args = (a.id,)))
